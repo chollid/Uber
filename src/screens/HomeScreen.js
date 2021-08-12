fcb6dbcdf24b615f7bf3f528357d40e798206ec1
chollid <chrisuga@me.com>
2021-08-11 21:58:31 -0400

    Change style

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -6,11 +6,11 @@ import NavOptions from "../components/NavOptions";
 const HomeScreen = () => {
   return (
     <SafeAreaView style={tw`bg-white h-full`}>
-      <View style={tw`p-7`}>
+      <View style={tw`p-5`}>
         <Image
           style={{
-            width: 110,
-            height: 110,
+            width: 100,
+            height: 100,
             resizeMode: "contain",
           }}
           source={{
